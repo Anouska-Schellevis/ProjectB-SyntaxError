7fc087442576a7d086bbdad386363f7124786cf4
JonathanHSR <1080312@hr.nl>
2024-10-23 09:59:36 +0200

    Reapply "Merge pull request #17 from data-mapping"
    This reverts commit 59c8accff3978ab92c1dd871f8324354008585a7.

---
+++ Project/DataAccess/ReservationAccess.cs
@@ -0,0 +1,36 @@
+using Microsoft.Data.Sqlite;
+
+using Dapper;
+
+
+public static class ReservationAccess
+{
+    private static SqliteConnection _connection = new SqliteConnection($"Data Source=DataSources/project.db");
+
+    private static string Table = "reservation";
+
+    public static void Write(ReservationModel reservation)
+    {
+        string sql = $"INSERT INTO {Table} (bar, seats_id, user_id, show_id) VALUES (@Bar, @SeatsId, @UserId, @ShowId)";
+        _connection.Execute(sql, reservation);
+    }
+
+    public static ReservationModel GetById(int id)
+    {
+        string sql = $"SELECT * FROM {Table} WHERE id = @Id";
+        return _connection.QueryFirstOrDefault<ReservationModel>(sql, new { Id = id });
+    }
+
+    public static void Update(ReservationModel reservation)
+    {
+        string sql = $"UPDATE {Table} SET bar = @Bar, seats_id = @SeatsId, user_id = @UserId, show_id = @ShowId WHERE id = @Id";
+        _connection.Execute(sql, reservation);
+    }
+
+    public static void Delete(int id)
+    {
+        string sql = $"DELETE FROM {Table} WHERE id = @Id";
+        _connection.Execute(sql, new { Id = id });
+    }
+
+}
