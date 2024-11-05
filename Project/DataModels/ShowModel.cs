863e5d76b9ed36f5c14633ba79231b9fc2035bec
JonathanHSR <1080312@hr.nl>
2024-11-05 11:05:32 +0100

    conflicts new main solved

--- Project/DataModels/ShowModel.cs
+++ Project/DataModels/ShowModel.cs
@@ -5,14 +5,8 @@ public class ShowModel
     public Int64 TheatreId { get; set; }
     public Int64 MovieId { get; set; }
     public string Date { get; set; }
-<<<<<<< HEAD
-=======
+    public ShowModel() { }
 
-<<<<<<< HEAD
-=======
-    //public ShowModel() { }
->>>>>>> 1a2c47d5e9d8eb777f4ca348b1db91d3b9379bf2
->>>>>>> test
     public ShowModel(Int64 id, Int64 theatre_id, Int64 movie_id, string date)
     {
         Id = id;
@@ -22,12 +16,8 @@ public class ShowModel
     }
 
 
-<<<<<<< HEAD
-}
-=======
 }
 
 
 
 
->>>>>>> test
