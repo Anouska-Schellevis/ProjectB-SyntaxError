27289b1175402b86249c0e802bc41a16010acd02
GenilleM <1054716@hr.nl>
2024-11-11 15:43:08 +0100

    Als het goed is zit de table er in

--- Project/Program.cs
+++ Project/Program.cs
@@ -1,6 +1,6 @@
 ﻿Console.WriteLine("Welcome to this amazing program");
 // Menu.Start();
 // Movie.Main();
-Show.UserStart();
+// Show.UserStart();
 //Show.Main();
-// Show.AdminStart();
+Show.AdminStart();
