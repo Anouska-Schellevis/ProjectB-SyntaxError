d0a2b5479ceb28dcd5b6cb3105e2b62f53766d1b
Anouska-Schellevis <anouskaschellevis@gmail.com>
2024-11-14 15:38:36 +0100

    Merge track-money with the main

--- Project/Program.cs
+++ Project/Program.cs
@@ -1,11 +1,11 @@
 ﻿Console.WriteLine("Welcome to this amazing program");
 Menu.Start();
 // Movie.Main();
-<<<<<<< HEAD
+
 // Show.UserStart();
-=======
+
 //Show.UserStart();
->>>>>>> ca7cddb (Split user and admin to seperate files)
+
 //Show.Main();
 // Show.AdminStart();
 
