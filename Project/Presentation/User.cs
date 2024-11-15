cb0a8024549cd92936ac300524c99807fb918121
JonathanHSR <1080312@hr.nl>
2024-11-15 11:27:10 +0100

    The user can sit max 2 hours at the bar after the movie

--- Project/Presentation/User.cs
+++ Project/Presentation/User.cs
@@ -8,8 +8,40 @@ class User
 
         List<ReservationModel> reservationsByUser = ReservationLogic.GetReservationsByUserId(currentUser.Id);
         
-        
+        // Merging reservations by ShowId to ensure that each show counts only once for a user,
+        // regardless of how many friends joined. This prevents duplicate warnings messages.
+        List<ReservationModel> mergedReservationsByUser = reservationsByUser
+        .GroupBy(b => b.ShowId)
+        .Select(group => new ReservationModel
+        {
+            ShowId = group.Key,
+            Id = group.First().Id,
+            Bar = group.First().Bar,  
+            UserId = group.First().UserId,
+            SeatsId = group.First().SeatsId
+        })
+        .ToList();
 
+        DateTime currentDateTime = DateTime.Now;
+        
+        foreach(ReservationModel reservation in mergedReservationsByUser)
+        {
+            ShowModel show = ShowLogic.GetByID(reservation.ShowId);
+            MoviesModel movie = MoviesLogic.GetById((int)show.MovieId);
+
+            DateTime movieBeginTime = DateTime.Parse(show.Date);
+            DateTime barReservationTimeStart = movieBeginTime.AddMinutes(movie.TimeInMinutes);
+            DateTime barReservationTimeEnd = barReservationTimeStart.AddHours(2);
+
+            System.Console.WriteLine(barReservationTimeEnd);
+            System.Console.WriteLine(currentDateTime);
+
+            if (barReservationTimeEnd < currentDateTime && currentDateTime < barReservationTimeEnd.AddMinutes(15)) // warning until 15 minutes after the end of the bar reservation
+            {
+                Console.WriteLine("Your bar reservation time is up. We kindly ask you to leave the bar");
+            }
+
+        }
 
 
         Console.WriteLine("Would you like to see the overview of available movies Y/N");
