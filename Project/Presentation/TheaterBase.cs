b485a2404c050ad32bb3a5487a4546a0ff7228d0
JonathanHSR <1080312@hr.nl>
2024-11-25 13:25:33 +0100

    Removed SetTakenSeats(showId) call

--- Project/Presentation/TheaterBase.cs
+++ Project/Presentation/TheaterBase.cs
@@ -112,13 +112,11 @@ public abstract class TheaterBase
     // Method to select seats
     public void SelectSeats(long showId, UserModel acc)
     {
-        SetTakenSeats(showId);
+
 
         List<long> reserved_seats = ReservationAccess.GetReservedSeatsByShowId(showId);
         UpdateSeatsArray(reserved_seats);
         DisplaySeats(showId);
-
-        char[,] lol = seats;
 
         Console.WriteLine("How many seats do you want to book?");
         int how_many_people = Convert.ToInt32(Console.ReadLine());
