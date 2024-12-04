80ca6ecffabf91543f009ee08f183166225d5daf
JonathanHSR <162428924+JonathanHSR@users.noreply.github.com>
2024-12-04 10:57:10 +0100

    bringing Program.cs up-to-date with main

--- Project/Program.cs
+++ Project/Program.cs
@@ -3,16 +3,6 @@ Console.Clear();
 Menu.Start();
 // Movie.Main();
 
-// UserModel acc = new(3, "test@gmail.com", "Testset", "Test", "Test", 611111111, 0, 0);
-
-// ConcreteTheater theater300 = (ConcreteTheater)Theater.GetTheater(300);
-// if (theater300 != null)
-// {
-//     theater300.SelectSeats(2, acc);
-// }
-
-// Show.UserStart();
-
 //Show.UserStart();
 
 //Show.Main();
