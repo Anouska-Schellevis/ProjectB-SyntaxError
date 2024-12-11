9750ab9ba237a979cfc3ac39e70066c956baa1ed
JonathanHSR <1080312@hr.nl>
2024-12-11 14:36:05 +0100

    added column expiration_date
    And adjusted the code accordingly

--- Project/Presentation/Voucher.cs
+++ Project/Presentation/Voucher.cs
@@ -92,7 +92,7 @@ class Voucher
                 Console.WriteLine($"Generated code {code}");
 
                 // A new voucher will be made and send to the create voucher function
-                VoucherModel newVoucher = new VoucherModel(0, code, description, amount, type, null);
+                VoucherModel newVoucher = new VoucherModel(0, code, description, amount, type, "2025-02-12 14:30", null);
                 VoucherLogic.CreateVoucher(newVoucher);
 
                 Console.WriteLine("Done!!");
