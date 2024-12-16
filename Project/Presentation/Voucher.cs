2551fffbea06466c8420c5eb339acad620efcde4
JonathanHSR <1080312@hr.nl>
2024-12-16 15:13:28 +0100

    Voucher adjustments
    - Print Expiration Dates
    - Removed menu with one option and go back to the voucher menu and then go back to  the user menu

--- Project/Presentation/Voucher.cs
+++ Project/Presentation/Voucher.cs
@@ -116,7 +116,8 @@ class Voucher
                     {
                         Console.WriteLine($"Amount: €{voucher.Amount},-");
                     }
-                    Console.WriteLine($"Description: {voucher.Description}\n---------------------------------------");
+                    Console.WriteLine($"Description: {voucher.Description}");
+                    Console.WriteLine($"Expiration Date: {voucher.ExpirationDate}\n---------------------------------------");
                     count += 1;
                 }
 
@@ -148,6 +149,7 @@ class Voucher
             case 1:
                 Console.Clear();
                 PrintAllUserVouchers(acc);
+                UserStart(acc);
                 break;
             case 2:
                 Console.Clear();
@@ -181,27 +183,9 @@ class Voucher
             {
                 Console.WriteLine($"Amount: €{voucher.Amount},-");
             }
-            Console.WriteLine($"Description: {voucher.Description}\n---------------------------------------");
+            Console.WriteLine($"Description: {voucher.Description}");
+            Console.WriteLine($"Expiration Date: {voucher.ExpirationDate}\n---------------------------------------");
         }
-        string choice;
-
-        do
-        {
-            Console.WriteLine("[1] Go back to voucher menu");
-            choice = Console.ReadLine();
-
-            if (choice == "1")
-            {
-                Console.Clear();
-                Voucher.UserStart(acc);
-            }
-            else
-            {
-                Console.WriteLine("Invalid input, please try again.");
-            }
-
-        } while (choice != "1");
-
     }
 
 }
